2148a45423dc6b62d9ef854d3cfe069e154b3c2a
lynch16 <will.lynch91@gmail.com>
2017-04-21 08:41:43 -0400

    using bower for dependency management. Stubbed tabs using ngmaterial

---
+++ js/app/app.js
@@ -0,0 +1,2 @@
+angular
+  .module('tsa', ['ngMaterial'])
---
+++ js/app/controllers/GraphsController.js
@@ -0,0 +1,17 @@
+function GraphsController($scope) {
+  var tabs = [
+      { title: 'One', content: 'Tab one content' },
+      { title: 'Two', content: 'Tab two content' }
+    ],
+    selected = null,
+    previous = null;
+  $scope.tabs = tabs;
+  $scope.selectedIndex = 1;
+  $scope.$watch('selectedIndex', (current, old) => {
+    previous = selected;
+    selected = tabs[current];
+  })
+}
+angular
+  .module('tsa')
+  .controller('GraphsController', GraphsController)
