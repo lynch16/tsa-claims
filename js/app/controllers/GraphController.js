d868d22d93ef28b79f7cea1681ae46936bacbb81
lynch16 <will.lynch91@gmail.com>
2017-04-24 18:42:53 -0400

    move data parsing function to service

--- js/app/controllers/GraphController.js
+++ js/app/controllers/GraphController.js
@@ -9,7 +9,7 @@ function GraphController($filter, ClaimsDataService, GraphService) {
 
       ctrl.years = Object.keys($filter('groupBy')(ctrl.values.claims, 'Incident Date'))
       ctrl.selectedYear = ctrl.selectedYear || ctrl.years[2]
-      groupedData = gatherClaimValues(groupedData)
+      groupedData = ClaimsDataService.gatherClaimValues(groupedData)
       let data = []
       for ( let group in groupedData ) {
         if (!!groupedData[group][ctrl.selectedYear]){
@@ -40,65 +40,6 @@ function GraphController($filter, ClaimsDataService, GraphService) {
     ctrl.loadGraph();
   }
 
-  const gatherClaimValues = (groupedData) => {
-    let result = {}
-    for (let groupKey in groupedData) {
-      let orderedGroup = $filter('groupBy')(groupedData[groupKey], 'Incident Date');  //order data by date for chart
-      for (let year in orderedGroup){
-        for (let month in orderedGroup[year]) {
-          let claimValues = []
-          for (let i = 0; i < orderedGroup[year][month].length; i++){
-            let val = parseFloat( orderedGroup[year][month][i]['Close Amount'].replace('$', "").trim() )
-            if (val > 0) {
-              claimValues.push(val);
-            }
-          }
-          if (claimValues.length > 0){
-            let avg = claimValues.reduce( (total, amt, index, array) => { //find average of that airline's monthly claim value
-              total += amt;
-              if (index === array.length-1){
-                return (total/array.length).toFixed(2)/1; //toFixed makes strings from floating decimals.
-                                                          //divide by 1 to make them numbers gain
-              } else {
-                return total;
-              }
-            });
-            if (groupKey === '-'){ //Fix '-' fields
-              groupKey = 'Unknown';
-            }
-            if (!result[groupKey]){
-              result[groupKey] = {
-                [year]: {
-                  [month]: avg
-                }
-              }
-            } else if (!result[groupKey][year]) {
-              result[groupKey][year] = {
-                  [month]: avg
-              }
-            } else {
-              result[groupKey][year][month] = avg
-            }
-          }
-        }
-      }
-    }
-    return result;
-  }
-
-
-  const parseData = () => {
-    let chartData = []
-    if (ctrl.type === 'line') {
-      let airlines = Object.keys(ctrl.values.avgTotalLoss)
-      airlines.forEach( (airline) => {
-        if (!!ctrl.values.avgTotalLoss[airline][ctrl.selectedYear]){ //if data for that airline exists
-          chartData.push( {[airline]: (ctrl.values.avgTotalLoss[airline][ctrl.selectedYear])} );
-        }
-      })
-    }
-    return chartData; //array of objects [ {Airline: ClaimVal } ]
-  }
   //
   // ctrl.$onChanges = () => {
   //   console.log('change');
--- js/app/services/ClaimsDataService.js
+++ js/app/services/ClaimsDataService.js
@@ -1,4 +1,4 @@
-function ClaimsDataService($q) {
+function ClaimsDataService($q, $filter) {
   let data = {
     claims: null,
   }
@@ -23,11 +23,58 @@ function ClaimsDataService($q) {
     return data;
   }
 
+  const gatherClaimValues = (groupedData) => {
+    let result = {}
+    for (let groupKey in groupedData) {
+      let orderedGroup = $filter('groupBy')(groupedData[groupKey], 'Incident Date');  //order data by date for chart
+      for (let year in orderedGroup){
+        for (let month in orderedGroup[year]) {
+          let claimValues = []
+          for (let i = 0; i < orderedGroup[year][month].length; i++){
+            let val = parseFloat( orderedGroup[year][month][i]['Close Amount'].replace('$', "").trim() )
+            if (val > 0) {
+              claimValues.push(val);
+            }
+          }
+          if (claimValues.length > 0){
+            let avg = claimValues.reduce( (total, amt, index, array) => { //find average of that airline's monthly claim value
+              total += amt;
+              if (index === array.length-1){
+                return (total/array.length).toFixed(2)/1; //toFixed makes strings from floating decimals.
+                                                          //divide by 1 to make them numbers gain
+              } else {
+                return total;
+              }
+            });
+            if (groupKey === '-'){ //Fix '-' fields
+              groupKey = 'Unknown';
+            }
+            if (!result[groupKey]){ //build or add to result object
+              result[groupKey] = {
+                [year]: {
+                  [month]: avg
+                }
+              }
+            } else if (!result[groupKey][year]) {
+              result[groupKey][year] = {
+                  [month]: avg
+              }
+            } else {
+              result[groupKey][year][month] = avg
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+
   return {
     data: data,
     getData: getData,
     loadData: loadData,
-    waitData: waitData
+    waitData: waitData,
+    gatherClaimValues: gatherClaimValues
   }
 }
 
