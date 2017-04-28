ac926604acc56f6efeba874fd4b6f43af6aba5de
lynch16 <will.lynch91@gmail.com>
2017-04-28 10:01:02 -0400

    custom filter to support displaying selected series

--- js/app/controllers/GraphController.js
+++ js/app/controllers/GraphController.js
@@ -11,14 +11,21 @@ function GraphController($filter, ClaimsDataService, GraphService) {
       let dateRange = GraphService.loadDateRange(ctrl.values.claims); //gather date range for all dates within dataset
       let groupedData = $filter('groupBy')(ctrl.values.claims, ctrl.groupType );  //returns object containing claims grouped by 2nd param
       let configuredData  = GraphService.configureValues(groupedData) //[ {[airline name]: { [month]: [claimValue, claimValue] }}, ... ]
-      let allSeries = GraphService.setSeries(configuredData);
-      ctrl.keys = ctrl.keys || allSeries;
-      configuredData = $filter('selectKeys')(configuredData, ctrl.keys)
+      ctrl.allSeries = GraphService.setSeries(configuredData);
+      ctrl.keys = [];
+      ctrl.checkAll = () => {
+        ctrl.keys = angular.copy(ctrl.allSeries);
+      }
+      ctrl.uncheckAll = () => {
+        ctrl.keys = [];
+      }
+
+      let filteredData = $filter('selectKeys')(configuredData, ctrl.keys);
 
       if (ctrl.type === 'line') {
         ctrl.labels = GraphService.setLabels(dateRange);
-        data = GraphService.setTotalValues(dateRange, configuredData);
-        series = GraphService.setSeries(configuredData);
+        data = GraphService.setTotalValues(dateRange, filteredData);
+        series = GraphService.setSeries(filteredData);
 
         let  averages = []  //calculate averages across all keys
         for (let i = 0; i < ctrl.labels.length; i++) {
@@ -42,9 +49,9 @@ function GraphController($filter, ClaimsDataService, GraphService) {
         }
       } else if (ctrl.type === 'bar') {
         ctrl.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        ctrl.series = GraphService.setSeries(configuredData);
+        ctrl.series = GraphService.setSeries(filteredData);
 
-        let data = GraphService.setCountAverages(ctrl.labels, configuredData);
+        let data = GraphService.setCountAverages(ctrl.labels, filteredData);
         ctrl.data = data[0]
         ctrl.options = {
           title: {
@@ -64,7 +71,6 @@ function GraphController($filter, ClaimsDataService, GraphService) {
     ctrl.groupOptions.forEach((option) => {
       ctrl.newClaim[option] = "";
     });
-
     ctrl.loadGraph();
     ctrl.refreshGraph = () => {     //needed in order to call from view
       ctrl.loadGraph();
@@ -74,6 +80,21 @@ function GraphController($filter, ClaimsDataService, GraphService) {
       ctrl.refreshGraph();
       console.log('New Claim Saved!');
     }
+  }
+
+  ctrl.$onChange = (changes) => {
+    console.log("changes");
+  }
+
+  ctrl.toggleKeys = (key) => {
+    console.log(key);
+    let indx = ctrl.keys.indexOf(key);
+    if (indx > -1) {
+      ctrl.keys.splice(indx, 1);
+    } else {
+      ctrl.keys.push(key);
+    }
+    console.log(ctrl.allSeries);
   }
 }
 
