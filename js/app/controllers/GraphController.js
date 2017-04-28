df3d8fbe5615a732885bdfe376b350b74c97a87c
lynch16 <will.lynch91@gmail.com>
2017-04-28 08:57:06 -0400

    added average values per month across all keys to line graph and moved dateRange function into service

--- js/app/controllers/GraphController.js
+++ js/app/controllers/GraphController.js
@@ -4,17 +4,31 @@ function GraphController($filter, ClaimsDataService, GraphService) {
   ctrl.loadGraph = () => {
     if (ctrl.values.claims !== null) {
       if (ctrl.type === 'line'){
-        ctrl.groupType = ctrl.groupType || "Airline Name"
+        ctrl.groupType = ctrl.groupType || "Airline Name" //default grouping for each graph
       } else {
         ctrl.groupType = ctrl.groupType || "Airport Code"
       }
-      let dateRange = loadDateRange(); //gather date range for all dates within dataset
+      let dateRange = GraphService.loadDateRange(); //gather date range for all dates within dataset
       let groupedData = $filter('groupBy')(ctrl.values.claims, ctrl.groupType );  //returns object containing claims grouped by 2nd param
       let configuredData  = GraphService.configureValues(groupedData) //[ {[airline name]: { [month]: [claimValue, claimValue] }}, ... ]
 
       if (ctrl.type === 'line') {
         ctrl.labels = GraphService.setLabels(dateRange);
-        ctrl.data = GraphService.setTotalValues(dateRange, configuredData);
+        series = GraphService.setSeries(configuredData);
+        data = GraphService.setTotalValues(dateRange, configuredData);
+
+        let  averages = []  //calculate averages across all keys
+        for (let i = 0; i < ctrl.labels.length; i++) {
+          let vals = []
+          data.forEach((dataset) => {
+            vals.push(parseFloat(dataset[i]));
+          });
+          averages.push(jStat.mean(vals).toFixed(2))
+        }
+        data.push(averages);
+        series.push("Global Average");
+        ctrl.data = data;
+        ctrl.series = series;
 
         ctrl.options = {
           title: {
@@ -25,6 +39,8 @@ function GraphController($filter, ClaimsDataService, GraphService) {
         }
       } else if (ctrl.type === 'bar') {
         ctrl.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        ctrl.series = GraphService.setSeries(configuredData);
+
         let data = GraphService.setCountAverages(ctrl.labels, configuredData);
         ctrl.data = data[0]
         ctrl.options = {
@@ -35,25 +51,7 @@ function GraphController($filter, ClaimsDataService, GraphService) {
           }
         }
       }
-
-      let series = GraphService.setSeries(configuredData);
-      ctrl.series = series
     }
-  }
-
-  const loadDateRange = () => {
-    let dates = Object.keys($filter('groupBy')(ctrl.values.claims, 'Incident Date'));
-    dates.sort((a, b) => {
-      date1 = new Date(parseInt(a));
-      date2 = new Date(parseInt(b));
-      if (date1 > date2) return 1;
-      if (date2 > date1) return -1;
-      return 0
-    });
-    let min = jStat.min(dates); //min of date range of claims
-    let max = jStat.max(dates);
-    let range = GraphService.allDatesInRange(min, max); //array of dates in milliseconds since epoch
-    return range;
   }
 
   ctrl.$onInit = () => {
--- js/app/services/GraphService.js
+++ js/app/services/GraphService.js
@@ -27,37 +27,37 @@ function GraphService($filter) {
     return extractedData
   }
 
-    const setCountAverages = (labels, data) => {
-      let avgCounts = [];
-      let stdDevs = [];
-      data.forEach((series) => {
-        let results = [ [], [] ]
-        for (let key in series) {  //key is airline, airport, etc.
-          for (let i = 0; i < labels.length; i++) { //for each month in labels
-            let monthlyCounts = [];
-            for (let date in series[key]) {         //check all claims
-              let month = new Date(parseInt(date)).getMonth();  //parse ms and store month
-              if (month === i){                                //if claim month and current label are equal
-                monthlyCounts.push(series[key][date].length); //add the number of claims
-              }
+  const setCountAverages = (labels, data) => {
+    let avgCounts = [];
+    let stdDevs = [];
+    data.forEach((series) => {
+      let results = [ [], [] ]
+      for (let key in series) {  //key is airline, airport, etc.
+        for (let i = 0; i < labels.length; i++) { //for each month in labels
+          let monthlyCounts = [];
+          for (let date in series[key]) {         //check all claims
+            let month = new Date(parseInt(date)).getMonth();  //parse ms and store month
+            if (month === i){                                //if claim month and current label are equal
+              monthlyCounts.push(series[key][date].length); //add the number of claims
             }
-            let avg, stdev;
-            if (monthlyCounts.length > 0) {
-              avg = jStat.mean(monthlyCounts).toFixed(2)
-              stdev = jStat.stdev(monthlyCounts).toFixed(2)
-            } else {                                          //keep zeros for months with no claims
-              avg = 0;
-              stdev = 0;
-            }
-            results[0].push(avg)
-            results[1].push(stdev)
-          } //end labels loop
-        }
-        avgCounts.push(results[0]) //array of averages
-        stdDevs.push(results[1]) //array of stdevs
-      });
-      return [avgCounts, stdDevs] //return as nested array per ChartJS
-    }
+          }
+          let avg, stdev;
+          if (monthlyCounts.length > 0) {
+            avg = jStat.mean(monthlyCounts).toFixed(2)
+            stdev = jStat.stdev(monthlyCounts).toFixed(2)
+          } else {                                          //keep zeros for months with no claims
+            avg = 0;
+            stdev = 0;
+          }
+          results[0].push(avg)
+          results[1].push(stdev)
+        } //end labels loop
+      }
+      avgCounts.push(results[0]) //array of averages
+      stdDevs.push(results[1]) //array of stdevs
+    });
+    return [avgCounts, stdDevs] //return as nested array per ChartJS
+  }
 
 
   const setLabels = (labels) => {
@@ -66,6 +66,21 @@ function GraphService($filter) {
       return (d.getMonth() + 1) + ", " + d.getFullYear();
     });
     return labels;
+  }
+
+  const loadDateRange = () => {
+    let dates = Object.keys($filter('groupBy')(ctrl.values.claims, 'Incident Date'));
+    dates.sort((a, b) => {
+      date1 = new Date(parseInt(a));
+      date2 = new Date(parseInt(b));
+      if (date1 > date2) return 1;
+      if (date2 > date1) return -1;
+      return 0
+    });
+    let min = jStat.min(dates); //min of date range of claims
+    let max = jStat.max(dates);
+    let range = allDatesInRange(min, max); //array of dates in milliseconds since epoch
+    return range;
   }
 
   const allDatesInRange = (min, max) => {
